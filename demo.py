15671a6e8cc22a02da233d7e1bf83c1d81079eb7
Alex5030 <89944759+Alex5030@users.noreply.github.com>
2026-01-17 01:17:52 +0100

    added classification model to demo. still needs to be revised

--- demo.py
+++ demo.py
@@ -1,5 +1,53 @@
 import cv2
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torch.optim as optim
+
+
+from data_import import train_images, test_images
+
+# still not done 
+# ------------------------- model definition , from classif.py
+num_classes = len(train_images.classes)
+idx_to_emotion = {
+    0: "surprise",    
+    1: "fear",        
+    2: "disgust",    
+    3: "happiness",   
+    4: "sadness",     
+    5: "anger",       
+}
+
+
+class EmotionCNN(nn.Module):
+    def __init__(self, num_classes=6):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2),  
+
+            nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2),  
+
+            nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2),  
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 8 * 8, 256),
+            nn.ReLU(),
+            nn.Dropout(0.0), 
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        return self.classifier(x)
+# -------------------------
+
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
