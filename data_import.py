b95ffaf48f4bd1634e64d40a51dfaade660db74b
Finnja Rauscher <finnrauscher9@gmail.com>
2026-01-25 10:41:03 +0100

    Deleted code for csv handeling, since labels get assigned through the folders tested, results in Sep_first_tests.pdf data_import is the code to be used, data_prep is only for testing other datasets

--- data_import.py
+++ data_import.py
@@ -1,7 +1,13 @@
 from torchvision import datasets, transforms
-import csv
 
 transform = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
+train_transform = transforms.Compose([ transforms.Resize((64, 64)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+    transforms.ToTensor(),
+])
+
 train_datapath = "data/train_images"
 test_datapath = "data/test_images"
 
@@ -9,27 +15,3 @@ test_datapath = "data/test_images"
 # Load Images
 train_images = datasets.ImageFolder(root=train_datapath, transform=transform,)
 test_images = datasets.ImageFolder(root=test_datapath, transform=transform)
-
-
-# Load Labels
-with open('data/test_labels.csv', mode='r', newline='') as file:
-    reader = csv.DictReader(file) 
-    test_labels = []
-    for row in reader:
-        filename = row['image']
-        label = row['label'] 
-        test_labels.append([label])
-
-with open('data/train_labels.csv', mode='r', newline='') as file:
-    reader = csv.DictReader(file) 
-    train_labels = []
-    for row in reader:
-        filename = row['image']
-        label = row['label'] 
-        train_labels.append([label])
-
-
-
-
-
-
--- data_prep.py
+++ data_prep.py
@@ -1,45 +1,9 @@
-import csv
 from PIL import Image
 from torchvision import transforms, datasets
 from pathlib import Path
 
 original_images_path = "archive(3)/train_images"        # add original image folder path
 resized_images_path  = "data_resized/train_images"  # add resized image folder path
-input_csv     = "archive(3)/labels.csv" # add original CSV path
-filtered_csv  = "formatted_output.csv"
-
-label_map = {
-    "surprise": 1,
-    "fear": 2,
-    "disgust": 3,
-    "happiness": 4,
-    "sadness": 5,
-    "anger": 6
-}
-exclude_labels = {"neutral", "content"}
-
-
-
-with open(input_csv, "r", newline="", encoding="utf-8") as infile, \
-     open(filtered_csv, "w", newline="", encoding="utf-8") as outfile:
-
-    reader = csv.DictReader(infile)
-    writer = csv.writer(outfile)
-    writer.writerow(["image", "label"])
-
-    for row in reader:
-        path = row["pth"]
-        folder = path.split("/")[0].strip().lower() if "/" in path else ""
-
-        if folder in exclude_labels:
-            continue
-
-        label_num = label_map.get(folder, "")
-        if label_num == "":
-            continue
-
-        writer.writerow([path, label_num])
-
 
 
 def resize_images(input_root, output_root, size=(64, 64)):
@@ -56,10 +20,7 @@ def resize_images(input_root, output_root, size=(64, 64)):
                     img = resize(img)
                     img.save(out_folder / f"{idx}.jpg")
 
-
 resize_images(original_images_path, resized_images_path)
-
-
 
 transform = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
 train_datapath = "archive(3)/train_images"
