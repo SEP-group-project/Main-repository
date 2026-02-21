6959046f6ca81c2b7d8dea0e73b2e2beb6ee8646
dorcas <chiuhsi605@gmail.com>
2026-02-21 23:17:08 +0100

    refactor imports

--- demo.py
+++ demo.py
@@ -4,179 +4,9 @@ import cv2
 import time
 from torchvision import models , transforms
 import torch.nn as nn
-from torch.optim import Adam
-import torch.optim as optim
-import torchvision.transforms as T
-import torchvision.transforms.functional as TF
-from torch.utils.data import DataLoader
-
-
-
-class EmotionCNN(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-
-        def block(in_ch, out_ch):
-            return nn.Sequential(
-                nn.Conv2d(in_ch, out_ch, 3, padding=1, bias=False),
-                nn.BatchNorm2d(out_ch),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(out_ch, out_ch, 3, padding=1, bias=False),
-                nn.BatchNorm2d(out_ch),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(2),
-                nn.Dropout2d(0.15),
-            )
-
-        self.features = nn.Sequential(
-            block(3, 32),
-            block(32, 64),
-            block(64, 128),
-            block(128, 256),
-        )
-
-        self.pool = nn.AdaptiveAvgPool2d(1)
-
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(256, 256),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(256, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.pool(x)
-        return self.classifier(x)
-    
-
-
-
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-preprocess = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-])
-
-
-
-
-
-def get_last_conv_layer(model):
-    
-    return model.features[3][3]
-
-
-
-
-def gradcam(model, face_bgr, class_idx):
-    
-    model.eval()
-
-    face_rgb = cv2.cvtColor(face_bgr, cv2.COLOR_BGR2RGB)
-    face_rgb = cv2.resize(face_rgb, (64, 64))
-    x = preprocess(face_rgb).unsqueeze(0).to(device)
-    
-    x.requires_grad_(True)
-    
-
-    conv_layer = get_last_conv_layer(model)
-    
-
-    activations = None
-    gradients = None
-    
-    def forward_hook(module, input, output): 
-        nonlocal activations
-        activations = output     #output: feature maps of the last conv-layer
-    
-    def backward_hook(module, grad_in, grad_out): 
-        nonlocal gradients
-        gradients = grad_out[0]  #gradient of feature maps
-
-    
-    hook_f = conv_layer.register_forward_hook(forward_hook)  
-    hook_b = conv_layer.register_full_backward_hook(backward_hook)
-
-    
-    preds = model(x)
-    score = preds[:, class_idx]  # score of the predicted class
-    model.zero_grad(set_to_none=True)
-    score.backward()
-
-    hook_b.remove()
-    hook_f.remove()
-
-    gradients = gradients.detach().cpu().numpy()[0]
-    activations = activations.detach().cpu().numpy()[0]
-
-    weights = np.mean(gradients, axis=(1,2)) # importance of each feature map 
-                                             # gradient.shape: (C, H, W)
-                                             # weights stores one weight per feature map
-
-    
-    heatmap = np.zeros(activations.shape[1:], dtype=np.float32)
-    for i, w in enumerate(weights):
-        heatmap += w*activations[i]   # one weighted feature map
-    
-    heatmap = np.maximum(heatmap, 0) # apply ReLU
-    heatmap /= heatmap.max()+ 1e-8   # normalize
-
-
-    return heatmap
-
-def overlay_heatmap(img, heatmap, alpha=0.4):
-    
-    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-    heatmap = np.uint8(255 * heatmap)
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-
-    superimposed_img = cv2.addWeighted(img, alpha, heatmap, 1-alpha, 0)
-    return superimposed_img
-
-
-
-from captum.attr import Saliency, NoiseTunnel
-import cv2
-import numpy
-from torchvision import transforms
-import torch
-import numpy as np
-
-
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-preprocess = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-])
-
-def coumpute_smoothGrad(model, img, target_class, samples):
-
-    
-    face_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    face_rgb = cv2.resize(face_rgb, (64, 64))
-    x = preprocess(face_rgb).unsqueeze(0).to(device)
-    
-    
-    
-    saliency = Saliency(model)
-    nt = NoiseTunnel(saliency)
-    attribution = nt.attribute(x, nt_type='smoothgrad',nt_samples=samples, target=target_class )
-
-    attr_np = attribution.squeeze().cpu().detach().numpy()
-    heatmap = np.sum(np.abs(attr_np), axis=0)
-    heatmap = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min() + 1e-8)
-    
-    return heatmap
-
-
+from model import EmotionCNN
+from xAI.gradcam import gradcam, overlay_heatmap
+from xAI.smoothGrad import coumpute_smoothGrad
 
 
 
@@ -301,63 +131,6 @@ def overlay_heatmap(img, heatmap, alpha=0.4):
 
 
 
-def occlusion_saliency(
-        model,
-        img,
-        target_class: int,
-        patch_size: int = 8,
-        stride: int = 4,
-        baseline: float = 0.0,
-        use_softmax = False,
-):
-
-    model.eval()
-
-    if img.dim() != 4 or img.shape[0] != 1:
-        raise ValueError("Image must have shape [1, C, H, W] ")
-
-    device = img.device
-    _, C, H, W = img.shape
-
-    out = model(img)
-    if use_softmax:
-        out = torch.softmax(out, dim=1)
-    base_score = out[0, target_class].item()
-
-    sal_sum = torch.zeros((H,W), device=device)
-    sal_cnt = torch.zeros((H,W), device=device)
-
-    for y in range(0, H, stride):
-        y1 = y
-        y2 = min(y + patch_size, H)
-
-        for x in range(0, W, stride):
-            x1 = x
-            x2 = min(x + patch_size, W)
-
-            occ = img.clone()
-            occ[:, :, y1:y2, x1:x2] = baseline
-
-            out_occ = model(occ)
-            if use_softmax:
-                out_occ = torch.softmax(out_occ, dim=1)
-            
-            occ_score = out_occ[0, target_class].item()
-            drop = base_score - occ_score
-            
-            sal_sum[y1:y2, x1:x2] += drop
-            sal_cnt[y1:y2, x1:x2] += 1.0
-
-    sal = sal_sum / torch.clamp(sal_cnt, min=1.0)
-    sal = torch.clamp(sal, min=0.0)
-
-    s_min, s_max = sal.min(), sal.max()
-    if (s_max > s_min) > 1e-12:
-        sal = (sal - s_min) / (s_max - s_min)
-    else:
-        sal = torch.zeros_like(sal)
-
-    return sal.detach().cpu()
 
 
 
@@ -422,8 +195,6 @@ def compute_xai_overlay(face_bgr, mode):
             heatmap = gradcam(model, face_bgr, pred_idx)
         elif mode == "smoothgrad":
             heatmap = coumpute_smoothGrad(model, face_bgr, pred_idx, samples=50)
-        elif mode == "occlusion":
-            heatmap = occlusion_saliency(model, face_bgr, pred_idx)
         elif mode == "activation":
             layer = get_conv_layer(model, which="last")
             activation = get_layer_activation(model, layer, x)
@@ -479,8 +250,8 @@ while True:
     key = cv2.waitKey(1) & 0xFF
 
     # --- XAI overlay keys ---
-    if key in [ord('g'), ord('s'), ord('v'), ord('o'), ord('a')]:
-        mode = {ord('g'):"gradcam", ord('s'):"smoothgrad", ord('o'):"occlusion", ord('a'):"activation"}[key]
+    if key in [ord('g'), ord('s'), ord('v'), ord('a')]:
+        mode = {ord('g'):"gradcam", ord('s'):"smoothgrad", ord('a'):"activation"}[key]
 
         if len(faces) > 0:
             x, y, w, h = faces[0]  # first face
@@ -497,7 +268,7 @@ while True:
 
     # Close overlay windows ---
     if key == ord('n'):
-        for win in ["GRADCAM Overlay","SMOOTHGRAD Overlay","OCCLUSION Overlay","ACTIVATION Overlay"]:
+        for win in ["GRADCAM Overlay","SMOOTHGRAD Overlay","ACTIVATION Overlay"]:
             cv2.destroyWindow(win)
 
     # Quit
